5802b7bfd4a51a4328fb6635f2951898cce7819a
boss <khiziouii@gmail.com>
2023-03-22 21:28:30 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,12 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-         if (b > largest) {
-           largest = b;
-          }
-         if (c > largest) {
-           largest = c;
-  }
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
 	return (largest);
 }
