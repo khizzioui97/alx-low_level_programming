644a2331adeb3243a4c30d991db73e515f7af841
boss <khiziouii@gmail.com>
2023-03-27 16:50:51 -0700

    fix 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,11 @@
  * @a: pointer to first value
  * @b: pointer to second value
  */
-
-void swap_int(int *a , int *b)
+void swap_int(int *a, int *b)
 {
-    int c = *a;
-    *a = *b;
-    *b = c;
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
